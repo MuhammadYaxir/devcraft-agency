2f8329fabd4372cce44372c3dc6378bf9520b592
Muhammad Yasir <73550266+MuhammadYaxir@users.noreply.github.com>
2026-05-20 11:23:11 +0500

    Contact page Navbar

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,6 @@
 import ContactFormSection from "@/sections/contact/ContactFormSection";
 import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function ContactPage() {
   return (
